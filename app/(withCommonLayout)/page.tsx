fdd438064afb679e989f1fbba9925c6167eef15e
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-07-27 15:20:46 +0600

    feat: basic layout for common & dashboard

---
+++ app/(withCommonLayout)/page.tsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+    return (
+        <>
+            Homepage
+            {/* <HeroSection />
+      <Specialist />
+      <TopRatedDoctors />
+      <WhyUs />
+      <HowItWorks />
+      <Stats /> */}
+        </>
+    );
+};
+
+export default HomePage;
