95be149c8b4dbe099611e6c9b0dbfa5909e60900
mehrabhossain1 <mehrab.munna00@gmail.com>
2025-11-27 14:36:06 +0600

    feat: Add WhoThisCourseIsFor component to outline target audience for the course

--- app/courses/research-foundation-&-scientific-literacy/page.tsx
+++ app/courses/research-foundation-&-scientific-literacy/page.tsx
@@ -6,6 +6,7 @@ import CourseModule from "./components/CourseModule";
 import WhatYouGet from "./components/WhatYouGet";
 import ResourcePersons from "./components/ResourcePersons";
 import WhatYouNeed from "./components/WhatYouNeed";
+import WhoThisCourseIsFor from "./components/WhoThisCourseIsFor";
 
 const ResearchFoundationScientificLiteracy = () => {
     const [isVideoPlaying, setIsVideoPlaying] = useState(false);
@@ -78,6 +79,9 @@ const ResearchFoundationScientificLiteracy = () => {
 
             {/* What You Need */}
             <WhatYouNeed />
+
+            {/* Who This Course is For */}
+            <WhoThisCourseIsFor />
 
             {/* Sticky Bottom Bar */}
             <StickyEnrollmentBar
