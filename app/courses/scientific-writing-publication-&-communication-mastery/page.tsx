b8346e185e602edaed9d718f97506ea368b08fc4
mehrabhossain1 <mehrab.munna00@gmail.com>
2025-11-27 16:14:42 +0600

    feat: Add FAQ component to address common questions about the course

--- app/courses/scientific-writing-publication-&-communication-mastery/page.tsx
+++ app/courses/scientific-writing-publication-&-communication-mastery/page.tsx
@@ -6,6 +6,7 @@ import CourseModule from "./components/CourseModule";
 import WhatYouGet from "./components/WhatYouGet";
 import ResourcePersons from "./components/ResourcePersons";
 import WhoThisCourseIsFor from "./components/WhoThisCourseIsFor";
+import FAQ from "./components/FAQ";
 
 const ScientificWritingPublicationCommunicationMastery = () => {
     const [isVideoPlaying, setIsVideoPlaying] = useState(false);
@@ -78,6 +79,9 @@ const ScientificWritingPublicationCommunicationMastery = () => {
 
             {/* Who This Course is For */}
             <WhoThisCourseIsFor />
+
+            {/* FAQ */}
+            <FAQ />
 
             {/* Sticky Bottom Bar */}
             <StickyEnrollmentBar
